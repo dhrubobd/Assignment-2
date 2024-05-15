a1fa4c25f76c56af17698a03d95315a8a7877317
Luke <luke.barishaldiocese@gmail.com>
2024-05-16 00:39:26 +0600

    Output structure is made

--- string.php
+++ string.php
@@ -1,1 +1,8 @@
 <?php
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach($strings as $string){
+    $vowelCount = 0;
+    $reverseString = strrev($string);
+    echo "Original String: {$string}, Vowel Count: {$vowelCount}, Reversed String: {$reverseString}".PHP_EOL;
+}
