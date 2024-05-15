86fb91e7f3e779d7195d23a33f67c120b7a7a4b3
Luke <luke.barishaldiocese@gmail.com>
2024-05-16 00:46:37 +0600

    checked the vowels in each string and counted

--- string.php
+++ string.php
@@ -3,6 +3,20 @@ $strings = ["Hello", "World", "PHP", "Programming"];
 
 foreach($strings as $string){
     $vowelCount = 0;
+    $length = strlen($string);
+    for($i=0;$i<$length;$i++){
+        if(strtolower($string[$i])=='a'){
+            $vowelCount++;
+        }elseif(strtolower($string[$i])=='e'){
+            $vowelCount++;
+        }elseif(strtolower($string[$i])=='i'){
+            $vowelCount++;
+        }elseif(strtolower($string[$i])=='o'){
+            $vowelCount++;
+        }elseif(strtolower($string[$i])=='u'){
+            $vowelCount++;
+        }
+    }
     $reverseString = strrev($string);
     echo "Original String: {$string}, Vowel Count: {$vowelCount}, Reversed String: {$reverseString}".PHP_EOL;
 }
